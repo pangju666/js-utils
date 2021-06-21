8b2805eb046c56d677b7c32526243b6ed4207db8
majiale <2281525307@qq.com>
2021-06-21 09:01:36 +0800

    重新修改配置文件

---
+++  .eslintrc.js
@@ -0,0 +1,17 @@
+﻿const path = require('path');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended' // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  }
+};
---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+﻿const path = require('path');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('@rollup/plugin-node-resolve');
+const merge = require('lodash.merge');
+const pkg = require('./package.json');
+
+const extensions = ['.js', '.ts'];
+
+const resolve = function(...args) {
+  return path.resolve(__dirname, ...args);
+};
+
+// 打包任务的个性化配置
+const jobs = {
+  esm: {
+    output: {
+      format: 'esm',
+      file: resolve(pkg.module),
+      sourcemap: true, //生成bundle.map.js文件，方便调试
+    },
+  },
+  umd: {
+    output: {
+      format: 'umd',
+      file: resolve(pkg.main),
+      name: 'rem',
+      sourcemap: true, //生成bundle.map.js文件，方便调试
+    },
+  },
+  min: {
+    output: {
+      format: 'umd',
+      file: resolve(pkg.main.replace(/(.\w+)$/, '.min$1')),
+      name: 'rem',
+      sourcemap: true, //生成bundle.map.js文件，方便调试
+    },
+    plugins: [],
+  },
+};
+
+// 从环境变量获取打包特征
+const mergeConfig = jobs[process.env.FORMAT || 'esm'];
+
+module.exports = merge(
+  {
+    input: resolve('./src/index.ts'),
+    output: {},
+    plugins: [
+      nodeResolve({
+        extensions,
+        modulesOnly: true,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+      }),
+    ],
+  },
+  mergeConfig,
+);
