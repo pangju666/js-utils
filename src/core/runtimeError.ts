df8b6584dafce620151985d308fe51389243fe4e
胖橘 <nullptr255@outlook.com>
2022-02-20 21:07:38 +0800

    refactor(DateUtils): 日期工具类重构完成

--- src/core/runtimeError.ts
+++ src/core/runtimeError.ts
@@ -36,3 +36,16 @@ export class IndexOutOfBoundsError extends Error {
         super(message);
     }
 }
+
+/**
+ * 表示解析时意外到达错误的信号。
+ *
+ * @author 胖橘
+ * @version 1.0
+ * @since 1.0
+*/
+export class ParseError extends Error {
+    constructor(message ?: string) {
+        super(message);
+    }
+}
