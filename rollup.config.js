7ad03aac86311496ca19246c432f108c4a82cb9d
majiale <2281525307@qq.com>
2022-06-24 18:23:50 +0800

    修改打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,15 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import {babel} from "@rollup/plugin-babel";
+import {terser} from "rollup-plugin-terser";
 import dts from "rollup-plugin-dts";
+
 import path from "path";
-import { babel } from "@rollup/plugin-babel";
 import pkg from "./package.json";
 
-const extensions = [".js", ".ts"];
-const resolvePath = (...args) => path.resolve(__dirname, ...args); // 适应不同环境，封装path.resolve，少写一点代码
+const resolvePath = (...args) => path.resolve(__dirname, ...args);
 
 export default [
-    {
-        input: resolvePath("./src/index.ts"),
-        output: {
-            name: "jsUtilsIife",
-            file: resolvePath("./", "lib/js-utils.iife.js"),
-            format: "iife",
-        },
-        plugins: [
-            babel({
-                babelHelpers: "runtime",
-                exclude: "node_modules/**",
-                extensions,
-            }),
-            commonjs({ extensions }),
-            nodeResolve({ extensions }),
-        ],
-    },
-    // esm，cjs格式打包
-    {
-        input: resolvePath("./src/index.ts"),
-        output: [
-            { file: resolvePath("./", pkg.exports.require), format: "cjs" },
-            { file: resolvePath("./", pkg.exports.import), format: "esm" },
-        ],
-        // external: ['date-fns'],
-        plugins: [
-            babel({
-                babelHelpers: "runtime",
-                exclude: "node_modules/**",
-                extensions,
-            }),
-            commonjs({ extensions }),
-            nodeResolve({ extensions }),
-        ],
-    },
     // 生成 .d.ts 类型声明文件
     {
         input: resolvePath("src/index.ts"),
@@ -52,5 +18,45 @@ export default [
             format: "es",
         },
         plugins: [dts()],
+    },
+    {
+        input: resolvePath("./src/index.ts"),
+        output: [
+            // cjs 格式打包
+            {file: resolvePath("./", pkg.main), format: "cjs"},
+            // es 格式打包
+            {file: resolvePath("./", pkg.module), format: "es"},
+            // umd 格式打包
+            {
+                file: resolvePath("./", pkg.unpkg),
+                format: "umd",
+                name: "PangJuUtils",
+                globals: {},
+                plugins: []
+            },
+        ],
+        plugins: [
+            // 解析第三方模块
+            nodeResolve({ modulesOnly: true, extensions: ['.js', '.ts']}),
+            // 配置babel
+            babel({
+                exclude: "**/node_modules/**",
+                babelHelpers: "runtime",
+                extensions: ['.js', '.ts'],
+            }),
+            // commonjs模块转es6模块
+            commonjs({extensions: ['.js', '.ts']}),
+            // 代码压缩
+            terser({
+                compress: {
+                    // 去除 console
+                    drop_console: true,
+                    pure_getters: true,
+                    unsafe: true,
+                    unsafe_comps: true,
+                    warnings: false
+                },
+            }),
+        ],
     },
 ];
