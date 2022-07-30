1f8eaffeae0855a9e0b6213d9369d8bbb8159151
胖橘 <nullptr255@outlook.com>
2022-07-30 18:05:11 +0800

    fix(index.ts): 修复某些工具类未导出问题

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 export { ArrayUtils } from "./util/ArrayUtils";
 export { BooleanUtils } from "./util/BooleanUtils";
 export { ConvertUtils } from "./util/ConvertUtils";
+export { ComparableUtils } from "./util/ComparableUtils";
 export { DateFormatUtils } from "./util/DateFormatUtils";
 export { DateUtils } from "./util/DateUtils";
 export { NumberUtils } from "./util/NumberUtils";
 export { ObjectUtils } from "./util/ObjectUtils";
 export { RandomUtils } from "./util/RandomUtils";
+export { RandomStringUtils } from "./util/RandomStringUtils";
+export { RegexUtils } from "./util/RegexUtils";
+export { TreeUtils } from "./util/TreeUtils";
 export { StringUtils } from "./util/StringUtils";
